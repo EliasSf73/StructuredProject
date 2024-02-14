f5ec8eff501ffe9b11e3868974c6dfa1e2331aa9
Elais Firisa <eliassfirisa@gmail.com>
2024-02-14 22:09:13 +0900

    mm

--- setup.py
+++ setup.py
@@ -2,25 +2,25 @@ from setuptools import find_packages,setup
 from typing import List
 
 hypen_e_dot='-e .'
-def get_requirements(filename:str)->List[str]:
-    'this will return list of requirements'
-    requirements=[]
-    with open(filename,'r') as file:
-        requirements=file.readlines()
-            # remove any leading or trailing white space
-        requirements=[req.strip() for req in requirements]
-        if hypen_e_dot in requirements:
-            requirements.remove(hypen_e_dot)
+from setuptools import setup
+
+def get_requirements(filename):
+    with open(filename, 'r') as file:
+        requirements = file.readlines()
+    # Remove any whitespace or newline characters
+    requirements = [line.strip() for line in requirements]
     return requirements
 
-
-
-
 setup(
-name='MLPROJECT',
-version='0.0.1',
-author='elias',
-author_email='eliassfirisa@gmail.com',
-install_requires=get_requirements('requirements.txt')
-description="This is an ML Project"
+    name='MLPROJECT',
+    version='0.0.1',
+    author='elias',
+    author_email='eliassfirisa@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    description="This is an ML Project"
 )
+
+
+
+
+
